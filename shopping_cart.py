38ce05b436dd6aa865964a8de2cf21fe917912ff
larazaki <lkz3@georgetown.edu>
2022-02-17 18:36:07 -0500

    Running Total Price

--- shopping_cart.py
+++ shopping_cart.py
@@ -39,6 +39,11 @@ def to_usd(my_price):
     """
     return f"${my_price:,.2f}" #> $12,000.71
 
+
+
+
+total_price = 0
+
 while True: 
 
     #TODO: write some Python code here to produce the desired output
@@ -61,7 +66,22 @@ while True:
     for x in products:
         if str(x["id"]) == str(product_id):
             matching_products.append(x)
-
-
+    
     matching_product = matching_products[0]
-    print(matching_product["name"], matching_product["price"])
+    total_price = total_price + matching_product["price"]
+
+
+
+
+
+
+
+
+# A grocery store name of your choice
+# A grocery store phone number and/or website URL and/or address of choice
+# The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
+# The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
+# The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
+# The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
+# The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
+# A friendly message thanking the customer and/or encouraging the customer to shop again
